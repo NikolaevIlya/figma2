2862909ec3ace158512cf99cfe6f88d0172645e0
Ulianov <dragonn050@mail.ru>
2021-09-22 12:23:24 +0300

    Initial Commit

---
+++ figma2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  figma2
+//
+//  Created by Преподаватель on 22.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
